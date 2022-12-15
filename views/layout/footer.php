d180886f74613311bdf6fd9c816fc4facee86f1b
David <99647909+DavNotDab@users.noreply.github.com>
2022-12-15 14:10:10 +0100

    Update footer.php

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -6,7 +6,7 @@
             <span>&copy; David Ballesteros Ortiz</span>
         </div>
         <div class="github">
-            <a href="https://github.com/DavNotDab">
+            <a href="https://github.com/DavNotDab/Senderismo">
                 <img src="https://www.shareicon.net/data/256x256/2015/08/10/82884_github_4096x4096.png" alt="logo_github" class="logo">
                 <span id="name">DavNotDab</span>
             </a>
